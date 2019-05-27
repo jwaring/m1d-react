23cd7656a02ec80644cc1b9efeaae3e2a9f3a1d5
Jason Waring <jason.waring@softwaringsolutions.com>
2019-05-27 13:09:51 +0800

    Updates

---
+++ precache-manifest.ebf5212b81c0cf095f380294f86722e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ad72457afdc51173edcd0ce7f46fad6",
+    "url": "/m1d-react/index.html"
+  },
+  {
+    "revision": "8757379bc3e44f573454",
+    "url": "/m1d-react/static/css/main.692eb38f.chunk.css"
+  },
+  {
+    "revision": "e04fe3269a09577a98f1",
+    "url": "/m1d-react/static/js/2.ef68b789.chunk.js"
+  },
+  {
+    "revision": "8757379bc3e44f573454",
+    "url": "/m1d-react/static/js/main.507799b1.chunk.js"
+  },
+  {
+    "revision": "43c527f8c8219bba4526",
+    "url": "/m1d-react/static/js/runtime~main.49352798.js"
+  }
+]);
